c9885549eef6703a07f4c1a854277a8c4e24a02c
NiixoZ <nicolas08.gay@gmail.com>
2023-10-23 18:36:42 +0200

    Update PrediGivree Layout

--- resources/js/Pages/Games/PrediGivreeIndex.jsx
+++ resources/js/Pages/Games/PrediGivreeIndex.jsx
@@ -1,5 +1,5 @@
 import { Head } from '@inertiajs/react';
-import GlobalLayout from '@/Layouts/GlobalLayout';
+import MainLayout from '@/Layouts/MainLayout';
 
 import MKTitle from '../../../assets/games/mario_kart_title.png'
 
@@ -8,11 +8,11 @@ export default function PrediGivreeIndex(props) {
     console.log(props)
 
     return (
-        <GlobalLayout>
+        <MainLayout>
             <Head title="Prédictions Givrées" />
             <div className="prediGivre hof">
                 <img width="650px" src={MKTitle} alt="MKTitle" />
             </div>
-        </GlobalLayout>
+        </MainLayout>
     );
 }
