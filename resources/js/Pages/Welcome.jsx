d65c6c816060a6be2611b06b72d2d341db587e00
Florent Calabresi <60184656+florentcalabresi@users.noreply.github.com>
2023-10-27 01:16:23 +0200

    Ajout du paramètre filtre dans l'accueil pour Prédigivre

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -19,7 +19,7 @@ export default function Welcome() {
                 <Head title="Accueil" />
                 <div className='flex flex-row w-full h-full justify-center items-center'>
                     <GameCard route={route('games.show', 'vipgames')} titleImg={VipGames} characterImg={VipGamesCharacter} coverImg={VipGamesCover}/>
-                    <GameCard route={route('predigivre.halloffame')} titleImg={MKGames} characterImg={MKCharacter} coverImg={MKCover}/>
+                    <GameCard route={route('predigivre.halloffame', {filter: 'today'})} titleImg={MKGames} characterImg={MKCharacter} coverImg={MKCover}/>
                 </div>
             </MainLayout>
         </>
