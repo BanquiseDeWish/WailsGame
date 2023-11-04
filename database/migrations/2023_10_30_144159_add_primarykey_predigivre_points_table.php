7822300b0209ce7cb4af6ff4c9e767eefc43c1ed
NiixoZ <nicolas08.gay@gmail.com>
2023-11-05 00:33:29 +0100

    Modify Migration

--- database/migrations/2023_10_30_144159_add_primarykey_predigivre_points_table.php
+++ database/migrations/2023_10_30_144159_add_primarykey_predigivre_points_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('predigivrees__points', function (Blueprint $table) {
-            $table->dropColumn('id');
             $table->increments('id')->before('user_id');
         });
     }
