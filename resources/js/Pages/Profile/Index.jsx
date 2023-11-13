7ef391dad1dcfaf846decae71deda8f73272e48d
Nico <nicolas08.gay@gmail.com>
2023-11-13 19:50:16 +0100

    Update Profile page

--- resources/js/Pages/Profile/Index.jsx
+++ resources/js/Pages/Profile/Index.jsx
@@ -1,14 +1,32 @@
 import MainLayout from "@/Layouts/MainLayout";
 import { Head } from "@inertiajs/react";
 
+import Penguin from "../../Components/Penguin";
+
 export default function ProfileIndex() {
 
     return (
         <MainLayout>
             <Head title="Profil" />
-            <div className="text-white">
-                This is profile index
+            <div className="info flex justify-center items-center w-full h-full">
+                Coming Soon...
+                <div className="absolute bottom-[-20%]">
+                    <Penguin size={{ width: 300, height: 400 }} />
+                </div>
             </div>
+
+            <style>
+            {`
+                .info {
+                    color: #FFF;
+                    font-family: Poppins;
+                    font-size: 3rem;
+                    font-style: italic;
+                    font-weight: 300;
+                    line-height: normal;
+                }
+            `}
+            </style>
         </MainLayout>
     )
 
