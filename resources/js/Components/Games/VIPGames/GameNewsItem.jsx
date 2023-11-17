ac09fc3aecb259193c232356fdf5e09a2f97762d
NiixoZ <nicolas08.gay@gmail.com>
2023-11-18 00:58:08 +0100

    Modify GameNewsItem css

--- resources/js/Components/Games/VIPGames/GameNewsItem.jsx
+++ resources/js/Components/Games/VIPGames/GameNewsItem.jsx
@@ -5,7 +5,7 @@ export default function GameNewsItem({userId, userName, subText, ...otherProps})
     const props = usePage().props;
     return (
         <>
-            <div className="game_news_item">
+            <div className="game_news_item container">
                 <img src={props.ziggy.url + '/api/user/' + userId + '/icon'} alt="" className='rounded-full h-[48px]' width={48}/>
                 <div className='flex flex-col gap-[0px] w-full overflow-hidden'>
                     <div className='item_username'>{userName}</div>
