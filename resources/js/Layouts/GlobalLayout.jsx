6ceec44816288dbd9d3c340ae1458e39b6029f86
Florent Calabresi <60184656+florentcalabresi@users.noreply.github.com>
2023-11-24 02:04:53 +0100

    Modification des HOF pour le mode mobile/tablet

--- resources/js/Layouts/GlobalLayout.jsx
+++ resources/js/Layouts/GlobalLayout.jsx
@@ -1,10 +1,10 @@
 import BgEffect from '../../assets/bg_effect.png'
 import { Toaster } from 'sonner'
-export default function GlobalLayout({ children }) {
+export default function GlobalLayout({ children, showOverflow }) {
     return (
         <>
             <Toaster />
-            <div className="flex w-screen h-screen flex-col items-center">
+            <div className={`flex w-screen h-screen flex-col items-center ${showOverflow ? "overflow-x-hidden overflow-y-auto xl:overflow-y-hidden" : "overflow-hidden"}`}>
                 <span id="app_background"/>
                 {children}
             </div>
