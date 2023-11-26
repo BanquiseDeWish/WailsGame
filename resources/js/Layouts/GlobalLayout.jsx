acf5c56326d61ae6f097d3234c9299b052675b97
Florent Calabresi <60184656+florentcalabresi@users.noreply.github.com>
2023-11-26 23:12:53 +0100

    Fix background Overlay

--- resources/js/Layouts/GlobalLayout.jsx
+++ resources/js/Layouts/GlobalLayout.jsx
@@ -7,6 +7,15 @@ export default function GlobalLayout({ children, showOverflow }) {
             <div className={`flex w-screen h-screen flex-col gap-0 items-center ${showOverflow ? "overflow-x-hidden overflow-y-auto" : "overflow-hidden"}`}>
                 {children}
             </div>
+            <style>
+                {
+                    `
+                        body {
+                            background-image: linear-gradient(254deg, #09101A 0%, #050A11 100%);
+                        }
+                    `
+                }
+            </style>
         </>
     );
 }
