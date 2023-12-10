7e542f66bae784c435e8d0a9fb68876b10884018
Florent Calabresi <60184656+florentcalabresi@users.noreply.github.com>
2023-12-10 13:22:10 +0100

    Ajout de la neige pour l'event Noel

--- resources/js/Layouts/GlobalLayout.jsx
+++ resources/js/Layouts/GlobalLayout.jsx
@@ -1,9 +1,11 @@
 import ToastContainer from '@/Components/Content/ToastContainer';
+import Snowfall from 'react-snowfall'
 import BgEffect from '../../assets/bg_effect.png'
 export default function GlobalLayout({ children, showOverflow }) {
     return (
         <>
             <div className={`flex w-screen h-screen flex-col gap-0 items-center ${showOverflow ? "overflow-x-hidden overflow-y-auto" : "overflow-hidden"}`}>
+                <Snowfall color="#fff" style={{ zIndex: 0 }} />
                 <ToastContainer />
                 {children}
             </div>
