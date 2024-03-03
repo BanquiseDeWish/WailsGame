1c8cfdbb412a7ce411e524023815e49f76851573
NiixoZ <nicolas08.gay@gmail.com>
2024-03-03 17:49:57 +0100

    Move care icon in InputDropdown comp

--- resources/js/Pages/Tools/ShinyWars/Modal/AddPokemonModal.jsx
+++ resources/js/Pages/Tools/ShinyWars/Modal/AddPokemonModal.jsx
@@ -78,10 +78,6 @@ export default class AddPokemonModal extends BaseModal {
                                 onChange={(value) => this.setState({ pokemonForm: value })}
                                 globalClassName='w-full z-40'
                                 autoComplete='off'
-                                selectIcon={<svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12.7 15H22.4895H31.3049C32.8134 15 33.5676 17.125 32.4991 18.3706L24.3594 27.8597C23.0552 29.3801 20.9339 29.3801 19.6297 27.8597L16.5341 24.2509L11.4901 18.3706C10.4373 17.125 11.1915 15 12.7 15Z" fill="#57779D"/>
-                                </svg>
-                                }
                             />
                             <GreenButton className='button_green outline-none w-fit'>Ajouter</GreenButton>
                         </div>
