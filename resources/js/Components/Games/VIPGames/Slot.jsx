496da01bb70746ce7b25c30103851ab7c56de7c8
Nico <nicolas08.gay@gmail.com>
2024-03-12 01:00:45 +0100

    :bug: Fix bug with the slot

--- resources/js/Components/Games/VIPGames/Slot.jsx
+++ resources/js/Components/Games/VIPGames/Slot.jsx
@@ -20,6 +20,7 @@ export default function Slot({ id, type, winner, spin, onClick, onSpinEnd, link,
     async function onNewData(data) {
         if(values.slot != undefined) {
             await waitUntil(() => !values.slot.isSpinning);
+            console.log("new data: ", data);
             values.slot.setData(data);
         }
     }
