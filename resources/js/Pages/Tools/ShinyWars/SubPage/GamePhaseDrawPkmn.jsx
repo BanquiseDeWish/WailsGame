e44b485f9aabc27705310cb717843341a86c9a41
Nico <nicolas08.gay@gmail.com>
2024-03-20 17:46:39 +0100

    :bulb: Comment GamePhaseDrawPkmn

--- resources/js/Pages/Tools/ShinyWars/SubPage/GamePhaseDrawPkmn.jsx
+++ resources/js/Pages/Tools/ShinyWars/SubPage/GamePhaseDrawPkmn.jsx
@@ -25,6 +25,8 @@ export default function GamePhaseDrawPkmn({ socket, globalValues, ...otherProps 
         <>
             <PokemonDrawModal data={globalValues.current?.drawpkm_player_choose} />
             <div className="flex flex-row gap-12">
+                
+                {/* Current Player & Teams */}
                 <div className="flex flex-col justify-between">
                     <div className="flex flex-row gap-2 justify-start items-center p-2 rounded container_background w-full">
                         <img width={56} src={player?.icon} alt="" className="rounded-full" />
@@ -61,6 +63,8 @@ export default function GamePhaseDrawPkmn({ socket, globalValues, ...otherProps 
                         }
                     </div>
                 </div>
+                
+                {/* Pokemon Types */}
                 <div className="grid grid-cols-6 container_background rounded-lg gap-8 p-8">
                     {globalValues.current?.pokemon_types?.map((type, index) => {
                         return (
