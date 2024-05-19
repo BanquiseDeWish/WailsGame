9136576c35a14ee6b6e0453a30fdbfdf30573bfa
NiixoZ <nicolas08.gay@gmail.com>
2024-05-20 01:47:04 +0200

    :sparkles: Add eyes cosmetics

--- resources/js/Pages/Profile/Appearance.jsx
+++ resources/js/Pages/Profile/Appearance.jsx
@@ -11,6 +11,7 @@ import BlueButton from "@/Components/Navigation/Buttons/BlueButton";
 import UserIcon from "@/Components/User/UserIcon";
 import LockIcon from "@/Components/Icons/LockIcon";
 import EmptyBoxIcon from "@/Components/Icons/EmptyBoxIcon";
+import { randomId } from "@/Game/random";
 
 
 export default function ProfileAppearance(props) {
@@ -39,7 +40,11 @@ export default function ProfileAppearance(props) {
                 },
                 {
                     name: 'Couleurs',
-                    key: 'color'
+                    key: 'penguin_color'
+                },
+                {
+                    name: 'Yeux',
+                    key: 'penguin_eye'
                 },
             ]
         },
@@ -173,15 +178,28 @@ export default function ProfileAppearance(props) {
                 <div className="container justify-start items-start xl:col-span-5 col-span-3 p-8 select-none overflow-y-auto">
                     {!cosmetics && <div className="flex justify-center items-center w-full h-full"><div className="loader-spinner"></div></div>}
                     <div className="flex flex-wrap gap-4">
-                        {cosmetics && cosmetics[0]?.sub_type == 'slogan' &&
+                        {cosmetics && activeTab == 'slogan' &&
                             <CosmeticCard height={128} key={'no_cosmetic'} onClick={() => { selectCosmetic(undefined) }}>
                                 <span className="flex items-center justify-center text-center h-full w-full">Un Pingouin Voyageur</span>
                             </CosmeticCard>
                         }
-                        {cosmetics && cosmetics[0]?.sub_type != 'slogan' &&
+                        {cosmetics && activeTab == 'penguin_eye' &&
                             <CosmeticCard height={200} key={'no_cosmetic'} onClick={() => { selectCosmetic(undefined) }}>
+                                <svg width={128} height={128}>
+                                    <g id="eye" transform="translate(-250,0)">
+                                        <path id="Vector_11" d="M288.77 27.6313C288.338 46.3919 291.648 68.8273 304.255 83.4833C308.6 87.9707 312.609 91.3304 319.033 91.6313C322.98 91.5842 325.139 90.7372 328.097 88.1436C331.959 84.1284 334.743 79.7328 336.966 74.6247C337.432 73.5695 337.432 73.5695 337.907 72.4929C339.184 67.0322 339.204 61.9018 336.366 56.9726C330.887 49.131 322.062 45.5862 313.566 42.0938C306.665 39.253 292.765 30.8921 288.77 27.6313Z" fill="#FAFAFB" />
+                                        <g id="pupil">
+                                            <circle id="Ellipse 8" cx="319.765" cy="68.3921" r="10" fill="#354357" />
+                                            <circle id="Ellipse 9" cx="323.265" cy="63.8921" r="3" fill="#EAEEF0" />
+                                        </g>
+                                    </g>
+                                </svg>
+                            </CosmeticCard>
+                        }
+                        {cosmetics && activeTab != 'slogan' && activeTab != 'penguin_eye' &&
+                            <CosmeticCard height={200} key={'no_cosmetic_1'} onClick={() => { selectCosmetic(undefined) }}>
                                 <div className={`flex justify-center items-end overflow-hidden w-[128px] h-[128px]`}>
-                                    <EmptyBoxIcon className="flex-shrink-0" width={96} height={96}/>
+                                    <EmptyBoxIcon className="flex-shrink-0" width={96} height={96} />
                                 </div>
                                 <span>Rien</span>
                             </CosmeticCard>
@@ -192,18 +210,20 @@ export default function ProfileAppearance(props) {
                                     case 'hat':
                                     case 'backpack':
                                     case 'accessory':
+                                    case 'penguin_eye':
                                         return (
-                                            <CosmeticCard key={cosmetic.name} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned}>
+                                            <CosmeticCard key={cosmetic.name+'_'+randomId()} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned}>
                                                 <div className={`flex justify-center items-end overflow-hidden w-[128px] h-[128px] ${!cosmetic.owned && 'opacity-70'}`}
                                                     dangerouslySetInnerHTML={{ __html: cosmetic.style }}
                                                 />
                                                 <span>{cosmetic.name}</span>
                                             </CosmeticCard>
                                         )
+                                    case 'penguin_color':
                                     case 'icon_background':
                                     case 'card_background':
                                         return (
-                                            <CosmeticCard key={cosmetic.name} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned}>
+                                            <CosmeticCard key={cosmetic.name+'_'+randomId()} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned}>
                                                 <div className={`flex justify-center items-end overflow-hidden w-[128px] h-[128px] rounded-full ${!cosmetic.owned && 'opacity-70'}`}
                                                     style={{ background: cosmetic.style }}
                                                 />
@@ -212,7 +232,7 @@ export default function ProfileAppearance(props) {
                                         )
                                     case 'slogan':
                                         return (
-                                            <CosmeticCard height={128} key={cosmetic.name} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned}>
+                                            <CosmeticCard height={128} key={cosmetic.name+'_'+randomId()} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned}>
                                                 <span className="flex justify-center items-center w-full h-full text-center">{cosmetic.name}</span>
                                             </CosmeticCard>
                                         )
