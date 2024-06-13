1846cae8bc0ced308b857171b81c0618d7c1f2c3
NiixoZ <nicolas08.gay@gmail.com>
2024-06-14 01:13:32 +0200

    :bug: Add name for all cosmetics

--- resources/js/Pages/Profile/Appearance/CosmeticList.jsx
+++ resources/js/Pages/Profile/Appearance/CosmeticList.jsx
@@ -9,12 +9,20 @@ export default function CosmeticList({ cosmetics, activeTab, selectCosmetic, act
             {!cosmetics && <div className="col-span-2 flex justify-center items-center w-full h-full"><div className="loader-spinner"></div></div>}
             {cosmetics && <div className="grid grid-cols-2 md:flex w-full md:flex-wrap gap-2 md:gap-4 overflow-y-scroll p-2 md:p-8 h-full content-start">
                 {cosmetics && activeTab == 'slogan' &&
-                    <CosmeticCard className={"md:h-[128px] md:w-[200px]"} key={'no_cosmetic'} onClick={() => { selectCosmetic(undefined) }}>
+                    <CosmeticCard c
+                        lassName={"md:h-[128px] md:w-[200px]"} 
+                        key={'no_cosmetic'} onClick={() => { selectCosmetic(undefined) }}
+                    >
                         <span className="flex items-center justify-center text-center h-full w-full">Un Pingouin Voyageur</span>
                     </CosmeticCard>
                 }
                 {cosmetics && activeTab == 'penguin_eye' &&
-                    <CosmeticCard className={"h-[160px] md:h-[200px] md:w-[200px]"} key={'no_cosmetic'} onClick={() => { selectCosmetic(undefined) }}>
+                    <CosmeticCard 
+                        className={"h-[160px] md:h-[200px] md:w-[200px]"} 
+                        key={'no_cosmetic'} 
+                        onClick={() => { selectCosmetic(undefined) }}
+                        name={"Par Défaut"}
+                    >
                         <div className={'scale-[0.75]'}>
                             <svg width={128} height={128}>
                                 <g id="eye" transform="translate(-250,0)">
@@ -29,11 +37,14 @@ export default function CosmeticList({ cosmetics, activeTab, selectCosmetic, act
                     </CosmeticCard>
                 }
                 {cosmetics && activeTab != 'slogan' && activeTab != 'penguin_eye' &&
-                    <CosmeticCard className={"h-[160px] md:h-[200px] md:w-[200px] justify-end"} key={'no_cosmetic_1'} onClick={() => { selectCosmetic(undefined) }}>
+                    <CosmeticCard 
+                        className={"h-[160px] md:h-[200px] md:w-[200px] justify-end"} 
+                        key={'no_cosmetic_1'} onClick={() => { selectCosmetic(undefined) }}
+                        name={"Rien"}
+                    >
                         <div className={`flex justify-center items-end overflow-hidden w-[96px] h-[96px]`}>
                             <EmptyBoxIcon className="flex-shrink-0" width={window.innerWidth <= 768 ? 64 : 96} height={window.innerWidth <= 768 ? 64 : 96} />
                         </div>
-                        <span>Rien</span>
                     </CosmeticCard>
                 }
                 {
@@ -42,25 +53,43 @@ export default function CosmeticList({ cosmetics, activeTab, selectCosmetic, act
                             case 'penguin_color':
                             case 'icon_background':
                                 return (
-                                    <CosmeticCard className={"h-[160px] md:h-[200px] md:w-[200px]"} key={cosmetic.name + '_' + randomId()} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned} active={activeCosmeticsIds?.includes(cosmetic?.id)}>
+                                    <CosmeticCard 
+                                        className={"h-[160px] md:h-[200px] md:w-[200px]"} 
+                                        key={cosmetic.name + '_' + randomId()} 
+                                        onClick={() => { selectCosmetic(cosmetic) }} 
+                                        lock={!cosmetic.owned} 
+                                        active={activeCosmeticsIds?.includes(cosmetic?.id)}
+                                        name={cosmetic.name}
+                                    >
                                         <div className={`flex flex-shrink-0 justify-center items-end overflow-hidden w-[80px] h-[80px] md:w-[96px] md:h-[96px] rounded-full ${!cosmetic.owned && 'opacity-70'}`}
                                             style={{ background: cosmetic?.style[0]?.style, backgroundSize: 'cover' }}
                                         />
-                                        <span>{cosmetic.name}</span>
                                     </CosmeticCard>
                                 )
                             case 'card_background':
                                 return (
-                                    <CosmeticCard className={"h-[160px] md:h-[200px] md:w-[200px]"} key={cosmetic.name + '_' + randomId()} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned} active={activeCosmeticsIds?.includes(cosmetic?.id)}>
+                                    <CosmeticCard 
+                                        className={"h-[160px] md:h-[200px] md:w-[200px]"} 
+                                        key={cosmetic.name + '_' + randomId()} 
+                                        onClick={() => { selectCosmetic(cosmetic) }} 
+                                        lock={!cosmetic.owned} 
+                                        active={activeCosmeticsIds?.includes(cosmetic?.id)}
+                                        name={cosmetic.name}
+                                    >
                                         <div className={`flex flex-shrink-0 justify-center items-end overflow-hidden w-[80px] h-[80px] md:w-[160px] md:h-[80px] rounded-lg ${!cosmetic.owned && 'opacity-70'}`}
                                             style={{ background: cosmetic?.style[0]?.style, backgroundSize: 'cover' }}
                                         />
-                                        <span>{cosmetic.name}</span>
                                     </CosmeticCard>
                                 )
                             case 'slogan':
                                 return (
-                                    <CosmeticCard className={"h-[128px] md:w-[200px]"} key={cosmetic.name + '_' + randomId()} onClick={() => { selectCosmetic(cosmetic) }} lock={!cosmetic.owned} active={activeCosmeticsIds?.includes(cosmetic?.id)}>
+                                    <CosmeticCard 
+                                        className={"h-[128px] md:w-[200px]"} 
+                                        key={cosmetic.name + '_' + randomId()} 
+                                        onClick={() => { selectCosmetic(cosmetic) }} 
+                                        lock={!cosmetic.owned} 
+                                        active={activeCosmeticsIds?.includes(cosmetic?.id)}
+                                    >
                                         <span className="flex justify-center items-center w-full h-full text-center">{cosmetic.name}</span>
                                     </CosmeticCard>
                                 )
